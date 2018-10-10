1c8c522a32ccd1ce990b20e1d02b01e05c62cd1e
akuinu <akuinumail@gmail.com>
2018-10-10 17:05:03 +0300

    made Settings class to seperate discord/twitch API interactions for local information storage and checks

--- discord-js/bot.js
+++ discord-js/bot.js
@@ -1,10 +1,100 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
 const http = require('http');
 const https = require('https');
 const regex = /(?:https\:\/\/)?(?:clips\.twitch\.tv)\/(\w*)/gmi;
 const fs = require('fs');
+
+class Settings{
+  //const fs = require('fs');
+  constructor(){
+    const config = require('./config.json');
+    this.discord_token = config.discord_token;
+    this.twitch_clientID = config.twitch_clientID;
+    this.rules = config.rules;
+  }
+
+  getDiscordToken(){
+    return this.discord_token;
+  }
+  getTwitchToken(){
+    return this.twitch_clientID;
+  }
+  inWatchList(channelID) {return this.rules.hasOwnProperty(channelID); }
+  isAllowedToPost(channelID, clipInfo){
+    if(clipInfo.hasOwnProperty('error')){
+      console.log("Invalid slug" + clipInfo);
+      return false;
+    } else {
+      return this.rules[channelID].allowedStreamers.indexOf(clipInfo.broadcaster.name) > -1;
+    }
+  }
+  getAllowedStreamersString(channelID){
+    return this.rules[channelID].allowedStreamers.join(', ');
+  }
+  info(channelID){
+    if (settings.inWatchList(channelID)) {
+      return `Channel is in watchlist. Allowed streamers: ${settings.getAllowedStreamersString(channelID)}`;
+    }
+    return "Current channel is not is watchlist.";
+  }
+
+  addStreamer(channelID, streamer){
+    streamer = streamer.trim().toLowerCase();
+    if (this.inWatchList(channelID)) {
+      if (this.rules[channelID].allowedStreamers.indexOf(streamer) == -1) {
+        this.rules[channelID].allowedStreamers.push(streamer);
+        this.saveConfig();
+        return `Streamer ${streamer} has been added to Allowed Streamers in this channel.`;
+      }
+      return `Streamer ${streamer} is alreaddy in Allowed Streamers in this channel.`;
+    }
+    return "This Channel is not in watchlist. Use !addChannel <streamer>";
+  }
+
+  removeStreamer(channelID, streamer){
+    streamer = streamer.trim().toLowerCase();
+    if (this.inWatchList(channelID)) {
+      var index = this.rules[channelID].allowedStreamers.indexOf(streamer);
+      if (index !== -1) {
+        this.rules[channelID].allowedStreamers.splice(index, 1);
+        this.saveConfig();
+        if (this.rules[channelID].allowedStreamers.length === 0) {
+          return `Streamer ${ streamer} was last allowed streamer, removed channel form watchlist.`;
+        }else {
+          return `Streamer ${streamer} has been removed to Allowed Streamers in this channel.`;
+        }
+      }
+      return `Streamer ${streamer}'s clips are not allowed here.`;
+    }
+    return "This Channel is not in watchlist.";
+  }
+
+  addChannel(channelID, streamer){
+    streamer = streamer.trim().toLowerCase();
+    if (!settings.inWatchList(channelID)) {
+      let rule = {"allowedStreamers":[streamer]};
+      this.rules[channelID] = rule;
+      this.saveConfig();
+      return `Channel added to watchlist. Allowed streamer: ${streamer}`;
+    }
+    return `Channel already in watchlist. Allowed streamers: ${settings.getAllowedStreamersString(channelID)} \n To add use !addStreamer <streamer>`;
+  }
+
+  removeChannel(channelID){
+    delete this.rules[channelID];
+    this.saveConfig();
+    return "Channel has been removed from the watchlist.";
+  }
+
+  saveConfig(){
+    var json = JSON.stringify(this);
+
+    fs.writeFile('./config.json', json, 'utf8', (error) => {console.log(error);  });
+  }
+}
+
+let settings = new Settings();
 const optionsTemplate = {
   host: 'api.twitch.tv',
   port: 443,
@@ -12,10 +102,9 @@ const optionsTemplate = {
   method: 'GET',
   headers: {
     'Accept': 'application/vnd.twitchtv.v5+json',
-    'Client-ID': config.twitch_clientID
+    'Client-ID': settings.getTwitchToken()
   }
 };
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -31,24 +120,24 @@ client.on('message', msg => {
           msg.reply('Pong!');
           break;
         case 'info':
-          msg.reply(info(msg.channel.id));
+          msg.reply(settings.info(msg.channel.id));
           break;
         case 'addStreamer':
-          msg.reply(addStreamer(msg.channel.id, args[0]));
+          msg.reply(settings.addStreamer(msg.channel.id, args[0]));
           break;
         case 'addChannel':
-          msg.reply(addChannel(msg.channel.id, args[0]));
+          msg.reply(settings.addChannel(msg.channel.id, args[0]));
           break;
         case 'removeStreamer':
-          msg.reply(removeStreamer(msg.channel.id, args[0]));
+          msg.reply(settings.removeStreamer(msg.channel.id, args[0]));
           break;
         case 'removeChannel':
-          msg.reply(removeChannel(msg.channel.id));
+          msg.reply(settings.removeChannel(msg.channel.id));
           break;
       }
     }
   } else {
-    if (inWatchList(msg.channel.id)) {
+    if (settings.inWatchList(msg.channel.id)) {
       var matches;
       while (matches = regex.exec(msg.content)) {
         var options = Object.assign({}, optionsTemplate);
@@ -63,7 +152,7 @@ client.on('message', msg => {
           });
           res.on('end', function() {
             var obj = JSON.parse(output);
-            if (!isAllowedToPost(msg.channel.id, obj)) {
+            if (!settings.isAllowedToPost(msg.channel.id, obj)) {
               msg.delete();
             }
            });
@@ -79,93 +168,8 @@ client.on('message', msg => {
   }
 });
 
-function inWatchList(channelID){
-  // TODO: make a proper dynamic system with DB
-  if (config.rules.hasOwnProperty(channelID)) {
-    return true;
-  }
-  return false;
-}
-
-function isAllowedToPost(channelID, clipInfo){
-  // TODO: make a proper dynamic system with DB and maybe allow server owner to post anything
-  if(clipInfo.hasOwnProperty('error')){
-    console.log("Invalid slug" + clipInfo);
-    return false;
-  } else {
-    return config.rules[channelID].allowedStreamers.indexOf(clipInfo.broadcaster.name) > -1;
-  }
-  return false;
-}
-
 function isServerOwner(msg){
   return msg.author.id === msg.channel.guild.ownerID;
 }
 
-function getAllowedStreamersString(channelID){
-  return config.rules[channelID].allowedStreamers.join(', ');
-}
-
-function info(channelID){
-  if (inWatchList) {
-    return "Channel is in watchlist. Allowed streamers: " + getAllowedStreamersString(channelID);
-  }
-  return "Current channel is not is watchlist.";
-}
-
-function addStreamer(channelID, streamer){
-  streamer = streamer.trim().toLowerCase();
-  if (inWatchList(channelID)) {
-    if (config.rules[channelID].allowedStreamers.indexOf(streamer) == -1) {
-      config.rules[channelID].allowedStreamers.push(streamer);
-      saveConfig();
-      return "Streamer " + streamer + " has been added to Allowed Streamers in this channel.";
-    }
-    return "Streamer " + streamer + " is alreaddy in Allowed Streamers in this channel.";
-  }
-  return "This Channel is not in watchlist. Use !addChannel <streamer>";
-}
-
-function removeStreamer(channelID, streamer){
-  streamer = streamer.trim().toLowerCase();
-  if (inWatchList(channelID)) {
-    var index = config.rules[channelID].allowedStreamers.indexOf(streamer);
-    if (index !== -1) {
-      config.rules[channelID].allowedStreamers.splice(index, 1);
-      if (config.rules[channelID].allowedStreamers.length === 0) {
-        removeChannel(channelID);
-        return "Streamer " + streamer + " was last allowed streamer, removed channel form watchlist.";
-      }else {
-        saveConfig();
-        return "Streamer " + streamer + " has been removed to Allowed Streamers in this channel.";
-      }
-    }
-    return "Streamer " + streamer + "'s clips are not allowed here.";
-  }
-  return "This Channel is not in watchlist.";
-}
-
-function addChannel(channelID, streamer){
-  streamer = streamer.trim().toLowerCase();
-  if (!inWatchList(channelID)) {
-    let rule = {"allowedStreamers":[streamer]};
-    config.rules[channelID] = rule;
-    saveConfig();
-    return "Channel added to watchlist. Allowed streamer: " + streamer;
-  }
-  return "Channel already in watchlist. Allowed streamers: " + getAllowedStreamersString(channelID) + "\n\r To add use !addStreamer <streamer>";
-}
-
-function removeChannel(channelID){
-  delete config.rules[channelID];
-  saveConfig();
-  return "Channel has been removed from the watchlist.";
-}
-
-function saveConfig(){
-  var json = JSON.stringify(config);
-
-  fs.writeFile('./config.json', json, 'utf8', (error) => {console.log(error);  });
-}
-
-client.login(config.discord_token);
+client.login(settings.getDiscordToken());
